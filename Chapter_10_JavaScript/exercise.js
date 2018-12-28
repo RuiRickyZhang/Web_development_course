52f35b780089b9cd27600013155fb0dd8fedbae8
ruizhang <ruizhang1995@tamu.edu>
2018-12-28 14:22:45 -0600

    JS Added

---
+++ Chapter_10_JavaScript/exercise.js
@@ -0,0 +1,7 @@
+var firstName = prompt("What\'s your firstName");
+var lastName = prompt("What\'s your lastName");
+var age = prompt("What\'s your age");
+console.log("Your full name is " + firstName + " "+ lastName);
+alert("Your full name is " + firstName + " "+ lastName);
+console.log("You are " + age + " years old");
+alert("You are " + age + " years old");
